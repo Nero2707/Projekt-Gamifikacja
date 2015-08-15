1e8ed6e813c8f3be744fe370817070cfef0f3cae
Michał Mazur <michal2707@o2.pl>
2015-08-15 19:09:47 +0200

    Dodanie parsera json. Napisanie czesci pozwalajacej na zalogowanie sie do aplikacji korzystajacej z zewnetrznej bazy danych

--- src/com/projekty/gamifikacjalublin/ekrany/MainActivity.java
+++ src/com/projekty/gamifikacjalublin/ekrany/MainActivity.java
@@ -1,30 +1,59 @@
 package com.projekty.gamifikacjalublin.ekrany;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.projekty.gamifikacjalublin.R;
 import com.projekty.gamifikacjalublin.R.id;
 import com.projekty.gamifikacjalublin.R.layout;
 import com.projekty.gamifikacjalublin.R.menu;
+import com.projekty.gamifikacjalublin.core.JSONParser;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener{
 	private Intent i;
+	private EditText user, pass;
+	private Button przyciskZaloguj, przyciskRejestruj;
+	private ProgressDialog pDialog;
+	
+	JSONParser jsonParser = new JSONParser();
+	
+	
+	 private static final String LOGIN_URL = "http://lublinquest.ugu.pl/webservice/login.php";
+	 private static final String TAG_SUCCESS = "success";
+	 private static final String TAG_MESSAGE = "message";
+	 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		View przyciskZaloguj = findViewById(R.id.przycisk_zaloguj);
+		user = (EditText)findViewById(R.id.pole_login);
+		pass = (EditText)findViewById(R.id.pole_haslo);
+		
+		przyciskZaloguj = (Button)findViewById(R.id.przycisk_zaloguj);
 		przyciskZaloguj.setOnClickListener(this);
 		
-		View przyciskRejestruj = findViewById(R.id.przycisk_zarejestruj);
+		przyciskRejestruj = (Button)findViewById(R.id.przycisk_zarejestruj);
 		przyciskRejestruj.setOnClickListener(this);
 	}
 
@@ -51,8 +80,9 @@ public class MainActivity extends Activity implements OnClickListener{
 	public void onClick(View v) {
 		switch(v.getId()){
 		case R.id.przycisk_zaloguj:
-			i = new Intent(this, MainMenu.class);
-			startActivity(i);
+			//i = new Intent(this, MainMenu.class);
+			//startActivity(i);
+			new AttemptLogin().execute();
 			break;
 		case R.id.przycisk_zarejestruj:
 			i = new Intent(this, RegisterMenu.class);
@@ -61,5 +91,71 @@ public class MainActivity extends Activity implements OnClickListener{
 		}
 		
 		
+	}
+	
+	
+	
+	
+	class AttemptLogin extends AsyncTask<String,String,String>{
+		
+		private boolean failure = false;
+		 
+		 
+		@Override
+	    protected void onPreExecute() {
+	        super.onPreExecute();
+	        pDialog = new ProgressDialog(MainActivity.this);
+	        pDialog.setMessage("Logowanie...");
+	        pDialog.setIndeterminate(false);
+	        pDialog.setCancelable(true);
+	        pDialog.show();
+	    }
+
+		@Override
+		protected String doInBackground(String... args) {
+			int success;
+			String username= user.getText().toString();
+			String password = pass.getText().toString();
+			try{
+				 // Parametryzowanie
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("password", password));
+
+                Log.d("request!", "starting");
+                // pobieranie danych uæytkownika przez httpRequest
+                JSONObject json = jsonParser.makeHttpRequest(
+                       LOGIN_URL, "POST", params);
+
+                
+                Log.d("Login attempt", json.toString());
+
+                // json success tag
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                	Log.d("Logowanie udane!", json.toString());
+                	//pDialog.dismiss();
+                	i = new Intent(MainActivity.this, MainMenu.class);
+    				startActivity(i);
+                	return json.getString(TAG_MESSAGE);
+                }else{
+                	Log.d("Logowanie nieudane!", json.getString(TAG_MESSAGE));
+                	return json.getString(TAG_MESSAGE);
+
+                }
+			}catch (JSONException e){
+				e.printStackTrace();
+			}
+	        return null;
+
+		}
+
+	    protected void onPostExecute(String file_url) {
+	    	 pDialog.dismiss();
+	    	 if (file_url != null){
+	            	Toast.makeText(MainActivity.this, file_url, Toast.LENGTH_LONG).show();
+	            }
+	    }
+
 	}
 }
